1314cd977aba3b372a11f74c4304d80e8e161a04
anjin-byte <thalesarkanzen@gmail.com>
2025-04-30 22:48:39 -0500

    added more microops - moved cycles_lookup to decode

--- sm83/src/cpu/decoder.rs
+++ sm83/src/cpu/decoder.rs
@@ -6,7 +6,7 @@ pub fn decode_instruction(opcode: u8, _rf: &RegisterFile) -> VecDeque<MicroOp> {
     match opcode {
         0x00 => VecDeque::new(), // NOP
         0x3E => VecDeque::from([
-            MicroOp::ReadImmediate8 { reg: Reg8::A },
+            MicroOp::ReadImmediate8 { into: Reg8::A },
         ]),
         _ => panic!("Unimplemented opcode: {:02X}", opcode),
     }
--- sm83/src/cpu/microcode.rs
+++ sm83/src/cpu/microcode.rs
@@ -2,37 +2,12 @@ use crate::cpu::CPU;
 
 use super::{alu::alu, registers::{Flag, Reg16, Reg8}};
 
-mod cycles {
-    const CYCLES_LOOKUP: [u8; 256] = [
-    // x0  x1  x2  x3  x4  x5  x6  x7  x8  x9  xA  xB  xC  xD  xE  xF
-        4, 12,  8,  8,  4,  4,  8,  4, 20,  8,  8,  8,  4,  4,  8,  4,  // 0x
-        4, 12,  8,  8,  4,  4,  8,  4,  8,  8,  8,  8,  4,  4,  8,  4,  // 1x
-        8, 12,  8,  8,  4,  4,  8,  4,  8,  8,  8,  8,  4,  4,  8,  4,  // 2x
-        8, 12,  8,  8, 12, 12, 12,  4,  8,  8,  8,  8,  4,  4,  8,  4,  // 3x
-        4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,  // 4x
-        4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,  // 5x
-        4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,  // 6x
-        8,  8,  8,  8,  8,  8,  4,  8,  4,  4,  4,  4,  4,  4,  8,  4,  // 7x
-        4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,  // 8x
-        4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,  // 9x
-        4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,  // Ax
-        4,  4,  4,  4,  4,  4,  8,  4,  4,  4,  4,  4,  4,  4,  8,  4,  // Bx
-        8, 12, 12, 12, 12, 16,  8, 32,  8,  8, 12,  8, 12, 12,  8, 32,  // Cx
-        8, 12, 12,  0, 12, 16,  8, 32,  8,  8, 12,  0, 12,  0,  8, 32,  // Dx
-        12, 12,  8,  0,  0, 16,  8, 32, 16,  4, 16,  0,  0,  0,  8, 32, // Ex
-        12, 12,  8,  4,  0, 16,  8, 32, 12,  8, 16,  4,  0,  0,  8, 32, // Fx
-    ];
-}
-
 #[derive(Debug)]
 pub enum MicroOp {
-    ReadImmediate8 { reg: Reg8 },
-    ReadImmediate16 { reg: Reg16 },
+    ReadImmediate8 { into: Reg8 },
+    ReadImmediate16 { into: Reg16 },
 
-    ReadReg8 { reg: Reg8 },
     WriteReg8 { dst: Reg8, val: u8 },
-
-    ReadReg16 { reg: Reg16 },
     WriteReg16 { dst: Reg16, val: u16 },
 
     IncrementReg8 { reg: Reg8 },
@@ -41,11 +16,7 @@ pub enum MicroOp {
     IncrementReg16 { reg: Reg16 },
     DecrementReg16 { reg: Reg16 },
 
-    ReadMem { addr: u16 },
     WriteMem { addr: u16, val: u8 },
-
-    // Indirect addressing through registers (like [HL])
-    ReadMemReg { addr_reg: Reg16 },
     WriteMemReg { addr_reg: Reg16, val: Reg8 },
 
     AluOp { kind: AluOpKind, lhs: Reg8, rhs: Operand8 },
@@ -77,20 +48,51 @@ pub enum Operand8 { Reg(Reg8), Imm(u8) }
 
 pub fn execute_microop(cpu: &mut CPU, op: MicroOp) {
     match op {
-        MicroOp::ReadImmediate8 { reg } => todo!(),
-        MicroOp::ReadImmediate16 { reg } => todo!(),
-        MicroOp::ReadReg8 { reg } => todo!(),
-        MicroOp::WriteReg8 { dst, val } => todo!(),
-        MicroOp::ReadReg16 { reg } => todo!(),
-        MicroOp::WriteReg16 { dst, val } => todo!(),
-        MicroOp::IncrementReg8 { reg } => todo!(),
-        MicroOp::DecrementReg8 { reg } => todo!(),
-        MicroOp::IncrementReg16 { reg } => todo!(),
-        MicroOp::DecrementReg16 { reg } => todo!(),
-        MicroOp::ReadMem { addr } => todo!(),
-        MicroOp::WriteMem { addr, val } => todo!(),
-        MicroOp::ReadMemReg { addr_reg } => todo!(),
-        MicroOp::WriteMemReg { addr_reg, val } => todo!(),
+        MicroOp::ReadImmediate8 { into: reg } => { // x2 memory access 
+            let pc = cpu.register_file.get_16bit(&Reg16::PC);
+            let val = cpu.bus.borrow_mut().read(pc);  // memory access
+            cpu.register_file.inc16(&Reg16::PC); 
+
+            cpu.register_file.set_8bit(&reg, val);
+        },
+        MicroOp::ReadImmediate16 { into: reg } => {
+            let pc = cpu.register_file.get_16bit(&Reg16::PC);
+            let lo = cpu.bus.borrow_mut().read(pc);
+            cpu.register_file.inc16(&Reg16::PC);
+
+            let pc = cpu.register_file.get_16bit(&Reg16::PC);
+            let hi = cpu.bus.borrow_mut().read(pc);  // memory access
+            cpu.register_file.inc16(&Reg16::PC);
+
+            let value = u16::from_le_bytes([lo, hi]);
+            cpu.register_file.set_16bit(&reg, value);
+        },
+        MicroOp::WriteReg8 { dst, val } => {
+            cpu.register_file.set_8bit(&dst, val);    
+        },
+        MicroOp::WriteReg16 { dst, val } => {
+            cpu.register_file.set_16bit(&dst, val);
+        },
+        MicroOp::IncrementReg8 { reg } => {
+            cpu.register_file.inc8(&reg);
+        },
+        MicroOp::DecrementReg8 { reg } => {
+            cpu.register_file.dec8(&reg);
+        },
+        MicroOp::IncrementReg16 { reg } => {
+            cpu.register_file.inc16(&reg);
+        },
+        MicroOp::DecrementReg16 { reg } => {
+            cpu.register_file.dec16(&reg);
+        },
+        MicroOp::WriteMem { addr, val } => {
+            cpu.bus.borrow_mut().write(addr, val);
+        }
+        MicroOp::WriteMemReg { addr_reg, val } => {
+            let addr = cpu.register_file.get_16bit(&addr_reg);
+            let v = cpu.register_file.get_8bit(&val);
+            cpu.bus.borrow_mut().write(addr, v);
+        }
         MicroOp::AluOp { kind, lhs, rhs } => {
             let a = cpu.register_file.get_8bit(&lhs);
             let b = match rhs {
@@ -110,8 +112,14 @@ pub fn execute_microop(cpu: &mut CPU, op: MicroOp) {
         MicroOp::UpdateFlags { result, kind } => todo!(),
         MicroOp::PushStack16 { src } => todo!(),
         MicroOp::PopStack16 { dst } => todo!(),
-        MicroOp::UpdatePC { addr } => todo!(),
-        MicroOp::AddRelativePC { offset } => todo!(),
+        MicroOp::UpdatePC { addr } => {
+            cpu.register_file.set_16bit(&Reg16::PC, addr);
+        }
+        MicroOp::AddRelativePC { offset } => {
+            let pc = cpu.register_file.get_16bit(&Reg16::PC);
+            let new_pc = pc.wrapping_add(offset as i16 as u16);
+            cpu.register_file.set_16bit(&Reg16::PC, new_pc);
+        }
         MicroOp::EnableInterrupts => todo!(),
         MicroOp::DisableInterrupts => todo!(),
         MicroOp::CheckInterrupts => todo!(),
